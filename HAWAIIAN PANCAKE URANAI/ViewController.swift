a8c6b995d6f199300249cf427a975938230f11a4
takareinhard <hokke.takara@gmail.com>
2019-09-01 19:29:02 +0900

    Initial Commit

---
+++ HAWAIIAN PANCAKE URANAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HAWAIIAN PANCAKE URANAI
+//
+//  Created by 弘田朗 on 2019/09/01.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HAWAIIAN PANCAKE URANAITests/HAWAIIAN_PANCAKE_URANAITests.swift
@@ -0,0 +1,34 @@
+//
+//  HAWAIIAN_PANCAKE_URANAITests.swift
+//  HAWAIIAN PANCAKE URANAITests
+//
+//  Created by 弘田朗 on 2019/09/01.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+import XCTest
+@testable import HAWAIIAN_PANCAKE_URANAI
+
+class HAWAIIAN_PANCAKE_URANAITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
